cdd7c29fa5a4733e8bd5b5e04abe92aa5326262b
Collin Boots <cboots@seas.upenn.edu>
2012-06-20 01:26:41 +0000

    Initial Project Module and drawings starting

---
+++ Drawing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace PCB_Layout_GA
+{
+    public abstract class Drawing
+    {
+        public abstract void draw(Graphics graphicsObj);
+    }
+
+    public class ArcDrawing : Drawing
+    {
+        public override void draw(Graphics graphicsObj)
+        {
+
+        }
+    }
+
+    public class SegmentDrawing : Drawing
+    {
+        public override void draw(Graphics graphicsObj)
+        {
+
+        }
+    }
+
+    public class CircleDrawing : Drawing
+    {
+        public override void draw(Graphics graphicsObj)
+        {
+
+        }
+    }
+}
---
+++ Module.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCB_Layout_GA
+{
+    class Module
+    {
+        public List<Pad> mPads;
+        public List<Drawing> mDrawings;
+
+
+        public class Pad
+        {
+            public enum PadType {CIRCLE, RECTANGULAR, OBLONG, TRAPEZE};
+            public enum PadAttr { STD, SMD, CONN, HOLE, MECA };
+
+            public int XSize { get; set; }
+            public int YSize { get; set; }
+            public int Orientation { get; set; }
+            public int XDelta { get; set; }
+            public int YDelta { get; set; }
+
+            public int NetNumber { get; set; }
+            public String NetName { get; set; }
+
+            public PadType Type { get; set; }
+            public PadAttr Attr { get; set; }
+            public int DrillSize { get; set; }
+            public int DrillXOffset { get; set; }
+            public int DrillYOffset { get; set; }
+
+        }
+
+
+    }
+}
