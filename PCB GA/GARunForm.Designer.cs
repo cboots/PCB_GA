6c3fc6e1ea584c4e9d9dfb71830fb9a092594aaa
Collin Boots <cboots@seas.upenn.edu>
2012-07-08 04:33:35 +0000

    Implemented Run with file

--- PCB GA/GARunForm.Designer.cs
+++ PCB GA/GARunForm.Designer.cs
@@ -29,22 +29,71 @@
         private void InitializeComponent()
         {
             this.gaBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.logBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
+            // 
+            // gaBackgroundWorker
+            // 
+            this.gaBackgroundWorker.WorkerReportsProgress = true;
+            this.gaBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.gaBackgroundWorker_DoWork);
+            this.gaBackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.gaBackgroundWorker_ProgressChanged);
+            this.gaBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.gaBackgroundWorker_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Running Genetic Algorithm";
+            // 
+            // logBox
+            // 
+            this.logBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logBox.Location = new System.Drawing.Point(12, 30);
+            this.logBox.Multiline = true;
+            this.logBox.Name = "logBox";
+            this.logBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logBox.Size = new System.Drawing.Size(415, 179);
+            this.logBox.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 215);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(415, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 3;
             // 
             // GARunForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 269);
+            this.ClientSize = new System.Drawing.Size(432, 250);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.progressBar1);
             this.Name = "GARunForm";
             this.Text = "GARunForm";
             this.Load += new System.EventHandler(this.GARunForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.ComponentModel.BackgroundWorker gaBackgroundWorker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox logBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- PCB GA/GARunForm.cs
+++ PCB GA/GARunForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using PCBGeneticAlgorithm;
+using System.IO;
 
 namespace PCB_Layout_GA
 {
@@ -22,7 +23,118 @@ namespace PCB_Layout_GA
 
         private void GARunForm_Load(object sender, EventArgs e)
         {
+            gaBackgroundWorker.RunWorkerAsync();
+        }
+
+        private void gaBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Setup first generation
+            GALayout[] generation = new GALayout[GA.GenerationSize];
+
+            logProgress("Creating Initial Population");
+            for (int i = 0; i < generation.Length; i++)
+            {
+                generation[i] = GALayout.GenerateRandomLayout(GA);
+            }
+
+            using (System.IO.StreamWriter fitnessWriter = new System.IO.StreamWriter(@"C:\Users\Collin\Dropbox\Current Classes\Independent Study\RunFitness.text"))
+            {
+                using (System.IO.StreamWriter f1Writer = new System.IO.StreamWriter(@"C:\Users\Collin\Dropbox\Current Classes\Independent Study\RunF1.text"))
+                {
+                    using (System.IO.StreamWriter f2Writer = new System.IO.StreamWriter(@"C:\Users\Collin\Dropbox\Current Classes\Independent Study\RunF2.text"))
+                    {
+                        using (System.IO.StreamWriter rawAreaWriter = new System.IO.StreamWriter(@"C:\Users\Collin\Dropbox\Current Classes\Independent Study\RunRawArea.text"))
+                        {
+                            using (System.IO.StreamWriter rawNetLengthWriter = new System.IO.StreamWriter(@"C:\Users\Collin\Dropbox\Current Classes\Independent Study\RunRawNetLength.text"))
+                            {
+                                for (int gen = 1; gen <= GA.MaxGeneration; gen++)
+                                {
+                                    logProgress((100 * gen) / GA.MaxGeneration, "Evaluating Generation: " + gen + "\n");
+                                    FitnessEvaluator.EvaluateGenerationFitness(GA, generation);
+
+                                    foreach (GALayout layout in generation)
+                                    {
+                                        fitnessWriter.Write(layout.Fitness);
+                                        fitnessWriter.Write(' ');
+                                        f1Writer.Write(layout.F1);
+                                        f1Writer.Write(' ');
+                                        f2Writer.Write(layout.F2);
+                                        f2Writer.Write(' ');
+                                        rawAreaWriter.Write(layout.RawAreaFitness);
+                                        rawAreaWriter.Write(' ');
+                                        rawNetLengthWriter.Write(layout.RawNetFitness);
+                                        rawNetLengthWriter.Write(' ');
+                                    }
+                                    fitnessWriter.WriteLine();
+                                    f1Writer.WriteLine();
+                                    f2Writer.WriteLine();
+                                    rawAreaWriter.WriteLine();
+                                    rawNetLengthWriter.WriteLine();
+
+
+                                    logProgress("Creating next generation\n");
+                                    generation = Selection.GenerateNextGeneration(GA, generation);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            using (System.IO.StreamWriter solutionWriter = new System.IO.StreamWriter(@"C:\Users\Collin\Dropbox\Current Classes\Independent Study\Solution.text"))
+            {
+                double minFitness = Double.MaxValue;
+                GALayout best = null;
+                for(int i = 0; i < generation.Length; i++)
+                {
+                    if(generation[i].Fitness < minFitness)
+                    {
+                        best = generation[i];
+                    }
+                }
+                print2DArray(best.GenerateArray(), solutionWriter);
+ 
+            }
+            logProgress("Run finished");
+        }
+
+        public static void print2DArray(ushort[,] array, StreamWriter writer)
+        {
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    writer.Write(array[j, i] + " ");
+                }
+                writer.WriteLine();
+            }
+        }
+
+        int mProgress = 0;
+        public void logProgress(string log)
+        {
+            logProgress(mProgress, log);
+        }
+
+        public void logProgress(int percent, string log)
+        {
+            mProgress = percent;
+            gaBackgroundWorker.ReportProgress(percent, log);
+        }
+
+        
+        private void gaBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+            if (e.UserState is string)
+            {
+                logBox.AppendText((string)e.UserState);
+            }
+        }
+
+        private void gaBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
 
         }
+
     }
 }
